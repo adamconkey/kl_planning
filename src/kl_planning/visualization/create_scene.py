7bc69f6c2fcf415be3d0a44e97e93a2b60161bda
Adam <adam.conkey@utah.edu>
2020-10-08 21:29:56 -0600

    Add agent

--- src/kl_planning/visualization/create_scene.py
+++ src/kl_planning/visualization/create_scene.py
@@ -15,15 +15,18 @@ class SceneManager:
         self.rate = rospy.Rate(rate)
         self.scene_msg = None
         self.start_goal_msg = None
+        self.agent_msg = None
         
         self.scene_pub = rospy.Publisher("/scene", MarkerArray, queue_size=1)
-        self.start_goal_pub = rospy.Publisher("/start", MarkerArray, queue_size=1)
+        self.start_goal_pub = rospy.Publisher("/start_goal", MarkerArray, queue_size=1)
+        self.agent_pub = rospy.Publisher("/agent", MarkerArray, queue_size=1)
 
     def create_scene(self, config_filename):
         file_util.check_path_exists(config_filename, "Scene configuration file")
         self.scene_config = file_util.load_yaml(config_filename)
         self.scene_msg = ros_util.get_marker_array_msg(self.scene_config['objects'])
         self.start_goal_msg = ros_util.get_marker_array_msg(self.scene_config['indicators'])
+        self.agent_msg = ros_util.get_marker_array_msg(self.scene_config['agents'])
                 
     def run(self):
         rospy.loginfo("Visualizing scene")
@@ -32,6 +35,8 @@ class SceneManager:
                 self.scene_pub.publish(self.scene_msg)
             if self.start_goal_msg:
                 self.start_goal_pub.publish(self.start_goal_msg)
+            if self.agent_msg:
+                self.agent_pub.publish(self.agent_msg)
             self.rate.sleep()
 
     def shutdown(self):
