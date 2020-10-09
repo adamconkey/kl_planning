6bab67edefbc6b6b78061e6124328066e7af8164
Adam <adam.conkey@utah.edu>
2020-10-08 21:21:19 -0600

    Add start and goal indicators

--- src/kl_planning/util/ros_util.py
+++ src/kl_planning/util/ros_util.py
@@ -1,6 +1,32 @@
 from visualization_msgs.msg import Marker, MarkerArray
 
 
+def get_marker_msg(obj_data, marker_id=0):
+    marker = Marker()
+    marker.id = marker_id
+    if obj_data['type'] == 'cube':
+        marker.type = Marker.CUBE
+    elif obj_data['type'] == 'cylinder':
+        marker.type = Marker.CYLINDER
+    marker.action = Marker.MODIFY
+    marker.pose.position.x = obj_data['position'][0]
+    marker.pose.position.y = obj_data['position'][1]
+    marker.pose.position.z = obj_data['position'][2]
+    marker.pose.orientation.x = obj_data['orientation'][0]
+    marker.pose.orientation.y = obj_data['orientation'][1]
+    marker.pose.orientation.z = obj_data['orientation'][2]
+    marker.pose.orientation.w = obj_data['orientation'][3]
+    marker.header.frame_id = obj_data['parent_frame']
+    marker.scale.x = obj_data['length']
+    marker.scale.y = obj_data['width']
+    marker.scale.z = obj_data['height']
+    marker.color.r = obj_data['color'][0]
+    marker.color.g = obj_data['color'][1]
+    marker.color.b = obj_data['color'][2]
+    marker.color.a = obj_data['color'][3]
+    return marker
+
+
 def get_marker_array_msg(objects):
     """
     Takes object configs as dictionary specifying pose, color, dimensions, type, etc. 
@@ -8,27 +34,7 @@ def get_marker_array_msg(objects):
     """
     array = MarkerArray()
     for i, (obj_id, obj_data) in enumerate(objects.items()):
-        marker = Marker()
-        marker.id = i
-        # TODO add other options and error check
-        if obj_data['type'] == 'cube':
-            marker.type = Marker.CUBE
-        marker.action = Marker.MODIFY
-        marker.pose.position.x = obj_data['position'][0]
-        marker.pose.position.y = obj_data['position'][1]
-        marker.pose.position.z = obj_data['position'][2]
-        marker.pose.orientation.x = obj_data['orientation'][0]
-        marker.pose.orientation.y = obj_data['orientation'][1]
-        marker.pose.orientation.z = obj_data['orientation'][2]
-        marker.pose.orientation.w = obj_data['orientation'][3]
-        marker.header.frame_id = obj_data['parent_frame']
-        marker.scale.x = obj_data['length']
-        marker.scale.y = obj_data['width']
-        marker.scale.z = obj_data['height']
-        marker.color.r = obj_data['color'][0]
-        marker.color.g = obj_data['color'][1]
-        marker.color.b = obj_data['color'][2]
-        marker.color.a = obj_data['color'][3]
+        marker = get_marker_msg(obj_data, i)
         array.markers.append(marker)
     return array
         
--- src/kl_planning/visualization/create_scene.py
+++ src/kl_planning/visualization/create_scene.py
@@ -14,18 +14,24 @@ class SceneManager:
     def __init__(self, rate=100):
         self.rate = rospy.Rate(rate)
         self.scene_msg = None
+        self.start_goal_msg = None
+        
         self.scene_pub = rospy.Publisher("/scene", MarkerArray, queue_size=1)
+        self.start_goal_pub = rospy.Publisher("/start", MarkerArray, queue_size=1)
 
     def create_scene(self, config_filename):
         file_util.check_path_exists(config_filename, "Scene configuration file")
-        scene_config = file_util.load_yaml(config_filename)
-        self.scene_msg = ros_util.get_marker_array_msg(scene_config['objects'])
-
+        self.scene_config = file_util.load_yaml(config_filename)
+        self.scene_msg = ros_util.get_marker_array_msg(self.scene_config['objects'])
+        self.start_goal_msg = ros_util.get_marker_array_msg(self.scene_config['indicators'])
+                
     def run(self):
         rospy.loginfo("Visualizing scene")
         while not rospy.is_shutdown():
             if self.scene_msg:
                 self.scene_pub.publish(self.scene_msg)
+            if self.start_goal_msg:
+                self.start_goal_pub.publish(self.start_goal_msg)
             self.rate.sleep()
 
     def shutdown(self):
